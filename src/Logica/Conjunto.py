69d56862ae9873da1fb44cca1352b48fb78d05aa
labf601 <74022578@continental.edu.pe>
2023-11-02 16:42:49 -0500

    Test aumentado

---
+++ src/Logica/Conjunto.py
@@ -0,0 +1,8 @@
+class Conjunto:
+    def __init__(self, conjunto):
+        self.__conjunto=conjunto
+
+
+    def promedio( self ):
+        return None
+
---
+++ tests/TestConjunto.py
@@ -0,0 +1,7 @@
+import unittest
+from src.Logica.Conjunto import Conjunto
+
+class TestConjunto( unittest.TestCase ):
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto=Conjunto([])
+        self.assertIsNone(conjunto.promedio())
