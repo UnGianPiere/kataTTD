b56d02fc97f7d118d3d64069a800a042e2f3b905
labf601 <74022578@continental.edu.pe>
2023-11-02 17:01:07 -0500

    test 3

--- tests/TestConjunto.py
+++ tests/TestConjunto.py
@@ -1,12 +1,16 @@
 import unittest
 from src.Logica.Conjunto import Conjunto
 
-class TestConjunto( unittest.TestCase ):
+
+class TestConjunto(unittest.TestCase):
     def test_conjunto_vacio_retornaNone(self):
-        conjunto=Conjunto([])
+        conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
 
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5])
+        self.assertEqual(5, conjunto.promedio())
 
-    def test_conjunto_unElemento_retornaValorUnicoElemento( self ):
-        conjunto=Conjunto([5])
-        self.assertEqual(5,conjunto.promedio())
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
+        conjunto = Conjunto([5, 7])
+        self.assertEqual(6, conjunto.promedio())
